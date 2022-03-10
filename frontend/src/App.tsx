dc79e6dc1f8dae81bfba75ccce2bb0bf3d5e2831
Luiz Carvalho <luiz.carvalho.lmc@gmail.com>
2022-03-10 19:25:25 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
-import Header from './components/Header'
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Listing from 'pages/Listing';
+import Form from 'pages/Form';
+import Header from "components/Header";
 
 function App() {
-  return ( 
-    <div>
-    <Header />
-    </div>
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
